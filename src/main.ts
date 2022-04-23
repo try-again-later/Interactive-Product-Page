90a216e55158cd016f32ffcd3cabcca6461abf3a
try-again-later <aa.popok@yandex.ru>
2022-04-24 00:25:13 +0300

    Try using requestAnimationFrame for carousel

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,10 @@ function getImageUrl(name: string, fileExtension: string = 'jpg') {
 document.addEventListener('alpine:initialized', () => {
   const images: NodeListOf<HTMLImageElement> = document.querySelectorAll('[data-product-image]');
   const urls = [
-    getImageUrl('product-4'),
     getImageUrl('product-1'),
     getImageUrl('product-2'),
     getImageUrl('product-3'),
     getImageUrl('product-4'),
-    getImageUrl('product-1'),
   ];
   images.forEach((image, i) => { image.src = urls[i]; });
 });
