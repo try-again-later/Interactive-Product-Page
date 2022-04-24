c2be273d44f3d16073b00b65ecaec3777bf90369
try-again-later <aa.popok@yandex.ru>
2022-04-24 16:23:38 +0300

    Implement adding products to shopping cart

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,6 @@
 declare module 'alpinejs';
 declare module '@alpinejs/focus';
+
+interface Window {
+  formatMoney: (amount: number, decimalPlaces?: number) => string;
+}
--- src/main.ts
+++ src/main.ts
@@ -18,5 +18,10 @@ document.addEventListener('alpine:initialized', () => {
   images.forEach((image, i) => { image.src = urls[i]; });
 });
 
+window.formatMoney = (amount: number, decimalPlaces: number = 2) => {
+  return (Math.round(amount * 100) / 100).toFixed(decimalPlaces);
+}
+
 Alpine.plugin(focus);
+
 Alpine.start();
