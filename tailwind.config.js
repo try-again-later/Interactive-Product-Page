f3a7fcbf940963fb9504259753ab965e5c468dee
try-again-later <aa.popok@yandex.ru>
2022-04-24 05:20:16 +0300

    Add shopping cart pop up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./src/**/*.{ts,html}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pulseScale: {
+          '0%': { transform: 'scale(1)' },
+          '40%': { transform: 'scale(1.25)' },
+          '100%': { transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        pulseScale: 'pulseScale 200ms ease-out forwards'
+      }
+    },
   },
   plugins: [],
-}
+};
