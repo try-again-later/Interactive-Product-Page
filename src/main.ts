609896486e69f8b361b2513e44fd1d36bc27994d
try-again-later <aa.popok@yandex.ru>
2025-06-13 23:00:54 +0300

    Make header elements responsive to large screens

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,9 @@ import './main.css';
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 
-function getImageUrl(name: string, fileExtension: string = 'jpg') {
-  return new URL(`./img/${name}.${fileExtension}`, import.meta.url).href
-}
-
-document.addEventListener('alpine:initialized', () => {
-  const images: NodeListOf<HTMLImageElement> = document.querySelectorAll('[data-product-image]');
-  const urls = [
-    getImageUrl('product-1'),
-    getImageUrl('product-2'),
-    getImageUrl('product-3'),
-    getImageUrl('product-4'),
-  ];
-  images.forEach((image, i) => { image.src = urls[i]; });
-});
-
 window.formatMoney = (amount: number, decimalPlaces: number = 2) => {
   return (Math.round(amount * 100) / 100).toFixed(decimalPlaces);
 }
 
 Alpine.plugin(focus);
-
 Alpine.start();
